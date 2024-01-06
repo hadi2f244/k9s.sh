72794b1917701f75c382540b39ebb0f1bc17b730
Hadi Azaddel <m.h.azaddel@gmail.com>
2024-01-06 13:11:40 +0330

    Update k9s.sh

--- k9s.sh
+++ k9s.sh
@@ -2,6 +2,6 @@
 
 download_link=$(curl -s https://api.github.com/repos/derailed/k9s/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep -i linux_amd6)
 wget -O k9s.tar.gz $download_link
-tar -zxf k9s.tar.gz k9s
-sudo mv k9s /usr/bin
+tar -zxf k9s.tar.gz
+sudo cp -r usr /usr
 rm k9s.tar.gz
