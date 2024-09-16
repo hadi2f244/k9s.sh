82c0228c127500cb1a08c2212c6029a0687e4a40
Hadi Azaddel <m.h.azaddel@gmail.com>
2024-09-16 11:13:16 +0330

    Update k9s.sh

--- k9s.sh
+++ k9s.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
 download_link=$(curl -s https://api.github.com/repos/derailed/k9s/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep -i "k9s_linux_amd64.deb" )
+echo $download_link
 wget $download_link
 sudo apt install -y ./k9s_linux_amd64.deb
 rm k9s_linux_amd64.deb
