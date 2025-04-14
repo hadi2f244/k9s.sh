639f413e791e1caa8bb1a3af71971b97144ee34d
Hadi Azaddel <m.h.azaddel@gmail.com>
2025-04-14 19:09:39 +0330

    Update helm.sh

--- helm.sh
+++ helm.sh
@@ -1,4 +1,12 @@
+#!/bin/bash -e
+
+function cleanup {
+  echo "Removing get_helm.sh"
+  rm get_helm.sh
+}
+trap cleanup EXIT
+
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
-rm get_helm.sh
+
