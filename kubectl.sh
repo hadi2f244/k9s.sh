6562cf8bbfeae45520af99da7b81a8f944c8a2d0
Hadi Azaddel <m.h.azaddel@gmail.com>
2025-04-14 19:16:05 +0330

    Update kubectl.sh

--- kubectl.sh
+++ kubectl.sh
@@ -1,10 +1,4 @@
-#!/bin/bash -e
-
-function cleanup {
-  echo "Removing kubectl"
-  rm kubectl
-}
-trap cleanup SIGINT
+#!/bin/bash
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
