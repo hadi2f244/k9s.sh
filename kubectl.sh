ef2965af10d13ebdc274cb1f462f639ef758bcd1
Hadi Azaddel <m.h.azaddel@gmail.com>
2025-04-14 19:12:21 +0330

    Update kubectl.sh

--- kubectl.sh
+++ kubectl.sh
@@ -5,6 +5,7 @@ function cleanup {
   rm kubectl
 }
 trap cleanup EXIT
+trap cleanup SIGINT
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
