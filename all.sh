422435424e9f5df132323e0bb2bec48e612ac3f8
Hadi Azaddel <m.h.azaddel@gmail.com>
2025-06-12 14:22:13 +0330

    Create all.sh

---
+++ all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+bash <(curl -Ls https://raw.githubusercontent.com/hadi2f244/k9s.sh/main/k9s.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/hadi2f244/k9s.sh/main/helm.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/hadi2f244/k9s.sh/main/kubectl.sh)
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+sudo mv kustomize /usr/local/bin/
